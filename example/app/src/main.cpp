863524e05345b503f19812b452fd0aa288bb117f
André Rudolph <andre.rudolph@zeiss.com>
2024-05-24 14:57:43 +0200

    feat: extend examples

--- example/app/src/main.cpp
+++ example/app/src/main.cpp
@@ -1,18 +1,56 @@
+#include <chrono>
+#include <cstdio>
+#include <future>
+#include <thread>
+
+#include <boost/signals2.hpp>
 #include <fmt/core.h>
 
 #include <utilities/Receiver.h>
 #include <utilities/Sender.h>
 
-#include <chrono>
-#include <cstdio>
-#include <future>
-#include <thread>
+using namespace boost::signals2;
+
+void
+hello()
+{
+  fmt::print(stderr, "Hello");
+}
+
+void
+world()
+{
+  fmt::print(stderr, ", world!\n");
+}
+
+void
+payload(std::string const& text)
+{
+  fmt::print(stderr, "{}", text);
+}
 
 int
 main()
 {
   using namespace example::utilities;
 
+  // example with void signal
+  signal<void()> s1;
+  s1.connect(1, world);
+  s1.connect(0, hello);
+  s1();
+  s1.disconnect(hello);
+  s1.disconnect(world);
+  s1();
+
+  // example with payload signal
+  signal<void(std::string const&)> s2;
+  s2.connect(&payload);
+  s2("First payload\n");
+  s2.disconnect(&payload);
+  s2("Second payload\n");
+
+  // example with countdown
   Sender sender{};
   Receiver receiver(sender);
 
@@ -27,7 +65,7 @@ main()
       std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
       if (progress == 10) {
-        fmt::print(stderr, "{}", progress);
+        fmt::print(stderr, "{}\n", progress);
 
         running = false;
       }
@@ -38,7 +76,7 @@ main()
 
   signalThread.wait();
 
-  fmt::print(stderr, "######### finished #########");
+  fmt::print(stderr, "######### finished #########\n");
 
   return EXIT_SUCCESS;
 }
